c48c4e8d1f6ca9aa73fd0e98224755bf87302c6c
Narita1910 <43438380+Narita1910@users.noreply.github.com>
2020-07-19 20:18:22 +0900

    Add files via upload

--- src/libquat/quaternion.cpp
+++ src/libquat/quaternion.cpp
@@ -4,6 +4,7 @@
 
 
 
+
 // コンストラクタ
 quaternion::quaternion()
 {
@@ -23,7 +24,10 @@ quaternion::quaternion(float x, float y, float z, float w)
 
 quaternion::quaternion(const vector3 &v, float arg)
 {
-	// todo: 実装して下さい
+	quaternion::x_ = v.x();
+	quaternion::y_ = v.y();
+	quaternion::z_ = v.z();
+	quaternion::w_ = arg;
 }
 
 // デストラクタ
@@ -43,14 +47,13 @@ quaternion quaternion::operator/(float f) const
 
 vector3 quaternion::operator*(const vector3& v) const
 {
-	// todo: 実装して下さい
-	return vector3();
+	return vector3(x_ * v.x(), y_ * v.y(), z_ * v.z());
 }
 
 quaternion quaternion::operator*(const quaternion& rhs) const
 {
-	// todo: 実装して下さい
-	return quaternion();
+	
+	return quaternion(x_ * rhs.x_, y_ * rhs.y_, z_ * rhs.z_, w_ * rhs.w_);
 }
 
 quaternion quaternion::operator+(const quaternion& rhs) const
@@ -62,7 +65,11 @@ quaternion quaternion::operator+(const quaternion& rhs) const
 // 単位元にする
 quaternion &quaternion::identity()
 {
-	// todo: 実装して下さい
+	
+	quaternion::x_ = 0;
+	quaternion::y_ = 0;
+	quaternion::z_ = 0;
+	quaternion::w_ = 1;
 	return *this;
 }
 
@@ -71,7 +78,7 @@ quaternion &quaternion::normalize()
 {
 	float l;
 	l = sqrt(x_ * x_ + y_ * y_ + z_ * z_ + w_ * w_);
-	quaternion::x_ = x_/l;
+	quaternion::x_ = x_ / l;
 	quaternion::y_ = y_ / l;
 	quaternion::z_ = z_ / l;
 	quaternion::w_ = w_ / l;
@@ -87,21 +94,42 @@ float quaternion::length_sq() const
 // 共役を返す
 quaternion quaternion::conjugate() const
 {
-	// todo: 実装して下さい
-	return quaternion();
+	return quaternion(-x_, -y_, -z_, w_);
 }
 
 // 逆元を返す
 quaternion quaternion::inverse() const
 {
-	// todo: 実装して下さい
-	return quaternion();
+	quaternion c = conjugate();
+	float leng = length_sq();
+	return quaternion(c.x_/leng, c.y_ / leng, c.z_ / leng, c.w_ / leng);
 }
 
 // 球面線形補間
 quaternion quaternion::slerp(const quaternion& q0, const quaternion& q1, float t)
 {
-	// todo: 実装して下さい
-	return quaternion();
-}
+	quaternion q;
+	float qr = q0.x_ * q1.x_ + q0.y_ * q1.y_ + q0.z_ * q1.z_+ q0.w_ * q1.w_ ;
+	float i = 1.0f - qr * qr;
 
+	if (i == 0.0f) {
+		q.x_ = q0.x_;
+		q.y_ = q0.y_;
+		q.z_ = q0.z_;
+		q.w_ = q0.w_;
+		return quaternion(q.x_,q.y_,q.z_,q.w_);
+	}
+	else {
+		float sp = sqrt(i);
+		float ph = acos(qr);
+		float pt = ph * t;
+		float t1 = sin(pt) / sp;
+		float t0 = sin(ph - pt) / sp;
+
+		q.x_ = q0.x_ * t0 + q1.x_ * t1;
+		q.y_ = q0.y_ * t0 + q1.y_ * t1;
+		q.z_ = q0.z_ * t0 + q1.z_ * t1;
+		q.w_ = q0.w_ * t0 + q1.w_ * t1;
+		return quaternion(q.x_, q.y_, q.z_, q.w_);
+	}
+}
